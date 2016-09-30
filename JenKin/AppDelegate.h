68f6d565db320651c5fab4d607bd73bd242a5710
zhoulk <849693228@qq.com>
2016-10-01 01:05:42 +0800

    first

---
+++ JenKin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenKin
+//
+//  Created by Leeesven on 16/10/1.
+//  Copyright © 2016年 Leeesven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenKin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenKin
+//
+//  Created by Leeesven on 16/10/1.
+//  Copyright © 2016年 Leeesven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
